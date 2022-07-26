655bcc2128d8887f0906a6e23acec56643a51c66
Hanan-Hussein <hanhuss035@gmail.com>
2022-07-26 16:23:43 +0300

    ft:colors-fix colors in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ module.exports = {
     extend: {
       colors:{
         brightRed:'hsl(12,88%,59%)',
-        brightRedLight:'hsl(12,88%,69)',
+        brightRedLight:'hsl(12,88%,69%)',
         brightRedSupLight:'hsl(12,88%,95%)',
-        darkBlue:'hsl(228,39%,23)',
-        darkGrayishBlue:'hsl(227,12%,61)',
-        veryDarkBlue:'hsl(228,39%,23)',
+        darkBlue:'hsl(228,39%,23%)',
+        darkGrayishBlue:'hsl(227,12%,61%)',
+        veryDarkBlue:'hsl(228,39%,23%)',
         veryPaleRed:'hsl(13,100%,96%)',
-        veryLightGray:'hsl(228,39%,23)',
+        veryLightGray:'hsl(0,0%,23%)',
 
       }
     },
